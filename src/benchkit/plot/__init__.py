004b2fa097fbd9917abf38adbbb2af20dbaa965f
Nathaniel Tornow <nathaniel.tornow@tum.de>
2025-11-23 12:48:49 +0100

    add line comparison

--- src/benchkit/plot/__init__.py
+++ src/benchkit/plot/__init__.py
@@ -3,7 +3,8 @@
 from __future__ import annotations
 
 from ._bar import bar_comparison
+from ._line import line_comparison
 from ._scatter import scatter_comparison
 from .pplot import pplot
 
-__all__ = ["bar_comparison", "pplot", "scatter_comparison"]
+__all__ = ["bar_comparison", "line_comparison", "pplot", "scatter_comparison"]
